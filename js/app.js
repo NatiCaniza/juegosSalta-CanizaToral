94c88774da10c1b4257512791dddbce426acb004
NataliaCaniza <natiect32@gmail.com>
2022-06-09 15:47:49 -0300

    incorporando eventos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
-//El usuario ingresa su nombre que es utilizado para darle la bienvenida
-let usuario = prompt('Por favor, ingrese su nombre').toUpperCase()
-alert('Bienvenido/a ' + usuario + ' a El Mundo del Juguete')
-
 //Funcion constructora de los objetos Juego
 class Juego {
-    constructor(nombre, precio,imgSrc) {
+    constructor(nombre, precio, imgSrc) {
         this.nombre = nombre
         this.precio = precio
         this.imgSrc = imgSrc
@@ -12,15 +8,15 @@ class Juego {
 }
 
 //Objetos creados con la funcion constructora
-const juego1 = new Juego ('LIFE: El juego de la vida', 4750,'img/lifeElJuegoDeLaVida.jpg')
-const juego2 = new Juego ('Monopoly', 5390,'img/monopoly.jpg')
-const juego3 = new Juego ('Preguntados: Realidad Aumentada', 3450,'img/preguntadosRealidadAumentada.jpg')
-const juego4 = new Juego ('El Juego de la Oca', 2500,'img/elJuegoDeLaOca.jpg')
-const juego5 = new Juego ('Carrera de Mente', 4500,'img/carreraDeMente.jpg')
-const juego6 = new Juego ('Cuatro en Línea', 1890,'img/4enLinea.jpg')
-const juego7 = new Juego ('Letras 3D', 1990,'img/letras3d.jpg')
-const juego8 = new Juego ('Batalla Naval', 1650,'img/batallaNaval.jpg')
-const juego9 = new Juego ('TEG Clásico', 5450,'img/tegClasico.jpg')
+const juego1 = new Juego('El juego de la vida', 4750, 'img/lifeElJuegoDeLaVida.jpg')
+const juego2 = new Juego('Monopoly Clásico', 5390, 'img/monopoly.jpg')
+const juego3 = new Juego('Preguntados', 3450, 'img/preguntadosRealidadAumentada.jpg')
+const juego4 = new Juego('El Juego de la Oca', 2500, 'img/elJuegoDeLaOca.jpg')
+const juego5 = new Juego('Carrera de Mente', 4500, 'img/carreraDeMente.jpg')
+const juego6 = new Juego('Cuatro en Línea', 1890, 'img/4enLinea.jpg')
+const juego7 = new Juego('Letras 3D', 1990, 'img/letras3d.jpg')
+const juego8 = new Juego('Batalla Naval', 1650, 'img/batallaNaval.jpg')
+const juego9 = new Juego('TEG Clásico', 5450, 'img/tegClasico.jpg')
 
 //Array de los objetos utilizados para agregarlos al carrito
 const listaJuegos = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, juego8, juego9]
@@ -28,90 +24,54 @@ const listaJuegos = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, jue
 //Array donde se agregan el o los juegos que el usuario selecciona
 let carrito = []
 
-const cardContainer = document.getElementById('cardContainer')
+//DOM
+const cardContainer = document.querySelector('#cardContainer')
 
 listaJuegos.forEach((elemento) => {
     const card = document.createElement('div')
     card.className = 'card'
     card.innerHTML = `
-    <img src="${elemento.imgSrc}" class="card-img-top container-fluid">
+    <img src="${elemento.imgSrc}" class="card-img-top">
     <h2 class="card-title">${elemento.nombre}</h2>
     <p class="card-text">$${elemento.precio}</p>
-    <button class="btn btn-primary"> Agregar al Carrito </button>
+    <button data-id="${elemento.nombre}"class="btn btn-primary"> Agregar al Carrito </button>
     `
     cardContainer.append(card)
 
-}
-)
+})
 
-//Funcion para armar el mensaje donde el usuario elige el o los juegos que va a comprar
-function armarMensaje(array) {
-    let mensaje = 'Indique con el número correspondiente el producto que desea comprar:'
-    let numero = 1
-
-    for( let i = 0; i <= array.length-1; i++) {
-        mensaje += '\n' + numero + '- ' + array[i].nombre + ' $' + array[i].precio;
-        numero++;
-    }
-    return mensaje
+//Funcion para agregar juegos al carrito
+const agregarJuego = (e) => {
+    const juegoElegido = e.target.getAttribute('data-id')
+    const elemento = listaJuegos.find((elemento) => elemento.nombre == juegoElegido)
+    carrito.push(elemento)
+    console.log(carrito)
 }
 
-//Funcion para mostrarle al usuario el listado de los juegos que estan en el carrito y que decida si comprar o no
-const juegosCarrito = () => {
-    let listaCarrito = ''
-    carrito.forEach((elemento) => {
-        listaCarrito += '\n' + '- ' + elemento.nombre;
-    })
-    return listaCarrito
-}
+//Botones Agregar al Carrito
+const botonesCompra = document.querySelectorAll('.btn-primary')
+botonesCompra.forEach((botonCompra) => {
+    botonCompra.addEventListener('click',agregarJuego)
+})
 
 // Funcion para calcular costo total del o los juegos seleccionados
-function sumaCarrito() {
+const sumaCarrito = () => {
     let sumaTotal = 0
     carrito.forEach((elemento) => {
-        sumaTotal += elemento.precio
+        sumaTotal = sumaTotal + elemento.precio
     })
-    return sumaTotal
+    alert('Su precio a pagar es de $' + sumaTotal)
+    alert('Gracias por su compra!')
 }
 
-//Variable y funcion creadas para que el usuario vea el primer juego que agregó al carrito y posteriormente pueda elegir si quiere agregar otro juego o no
-let juegoSeleccionado
+//Boton Finalizar Compra
+const finalizarCompra = document.querySelectorAll('.finalizarCompra')
+finalizarCompra.forEach((botonFinalizar) => {
+    botonFinalizar.addEventListener('click', sumaCarrito)
+})
 
-function seleccionUsuario(producto, precio) {
-    juegoSeleccionado = producto
-    alert(usuario + ' usted seleccionó: ' + producto + ' a $' + precio)
-}
 
-//Funcion que utilizo para agregar juegos al carrito
-const agregarJuego = () => {
-    let mensajeUsuario = armarMensaje(listaJuegos)
-    let numeroJuego = Number((prompt(mensajeUsuario)))
-    seleccionUsuario(listaJuegos[numeroJuego-1].nombre, listaJuegos[numeroJuego-1].precio)
-    carrito.push(listaJuegos.splice(numeroJuego-1,1)[0])
-}
 
-agregarJuego()
-
-//Sirve para que el usuario pueda decidir si agrega otro juego a su compra
-while (confirm ('Desea agregar otro juego?')) {
-    agregarJuego()
-} 
-//Si el usuario no agrega otro juego le muestra solo el juego que selecciono junto con el precio total de la compra y pregunta si quiere comprar o no, dando como respuesta dos mensajes diferentes segun la eleccion que haga el usuario
-if (carrito.length === 1) {
-    if (confirm('Desea completar su compra de ' + juegoSeleccionado + '?')) {
-        alert('Su total es de: $' + sumaCarrito() +
-            '\n Gracias por su compra!')
-    } else {
-        alert('Gracias por su visita!')
-    }
-} else {
-    if (confirm ('Desea completar su compra de los siguientes juegos:' + juegosCarrito())) {
-        alert('Total de la compra: $' + sumaCarrito() + 
-        '\n Gracias por su compra!')
-    } else {
-        alert('Gracias por su visita!')
-    }
-}  //Si el usuario agrega mas de un juego al carrito, le muestra un listado con los juegos seleccionados junto con el precio total de la compra y pregunta si quiere comprar o no, dando como respuesta dos mensajes diferentes segun la eleccion que haga el usuario
 
 
 
